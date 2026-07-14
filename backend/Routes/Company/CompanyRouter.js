0cf25ee770646e60d0fb0ac31ec78a60ca1857ee
Gopal413 <dhangarg413@gmail.com>
2026-07-15 00:53:32 +0530

    updates

--- backend/Routes/Company/CompanyRouter.js
+++ backend/Routes/Company/CompanyRouter.js
@@ -2,13 +2,13 @@
 
 const express = require('express');
 const isAdmin = require('../../MiddleWare/adminAuth');
-const {createCompany,updateCompany,getCompany,deleteCompany} = require("../../Controllers/CompanyControllers/CompanyDetails")
+const { createCompany, updateCompany, getCompany, deleteCompany } = require("../../Controllers/CompanyControllers/CompanyDetails")
 const router = express.Router();
- 
-const upload = require("../../Config/multerConfig")
-router.post("/add",isAdmin,upload.single("file"),createCompany );
+
+const upload = require("../../config/multerConfig")
+router.post("/add", isAdmin, upload.single("file"), createCompany);
 router.put("/update/:id", isAdmin, upload.single("file"), updateCompany);
-router.put("/update:id",  isAdmin, updateCompany);
+router.put("/update:id", isAdmin, updateCompany);
 router.delete("/delete/:id", isAdmin, deleteCompany);
 
 // Customer can view
